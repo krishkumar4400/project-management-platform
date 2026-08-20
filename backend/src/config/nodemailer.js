a2ac01748aa08e600b0a8d56032d9cbafaf4d1ed
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-08-20 15:32:20 +0530

    Add database design schema for users, projects, tasks, and related entities
    Created tables for users, projects, project members, tasks, subtasks, and notes.
    
    Defined relationships between tables including foreign keys and enums for roles and task statuses.
    
    Included fields for user authentication and project management.

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -1,14 +1,14 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: 'OAuth2',
-        user: '',
-        clientId: '',
-        clientSecret: '',
-        refreshToken: ''
-    }
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: GOOGLE_USER,
+    clientId: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    refreshToken: GOOGLE_CLIENT_REFRESH_TOKEN,
+  },
 });
 
 export default transporter;
